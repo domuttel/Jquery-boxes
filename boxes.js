93875fd14edb0c238a6f086f28b8e194a0ceb726
Dominic Muttel <dominic.muttel@gmail.com>
2015-08-07 16:29:51 -0600

    Update boxes.js

--- boxes.js
+++ boxes.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   console.log("Ready for DOM manipulation");
-  1. Find the secretBox on the page. Set the background color to white.  Add an h1 tag that says, "secret box!" FINISHED
+ // 1. Find the secretBox on the page. Set the background color to white.  Add an h1 tag that says, "secret box!" FINISHED
   $('#secretBox').css({ "background-color": "white"}).html("<h1>secret box</h1>").css("color", "green");
 
   // 2. Find all child divs of the first row.  Set the class for each div to boxType3. FINISHED
